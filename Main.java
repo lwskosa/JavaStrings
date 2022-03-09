8b2468d5f412b1a27baaa4f84d5bf5e446dca7f1
lwskosa <lwskosa@gmail.com>
2022-03-09 12:25:48 -0600

    Fix code formatting, add comments

--- Main.java
+++ Main.java
@@ -3,22 +3,31 @@ package com.techbee.lajoskosa.javastrings;
 public class Main {
 
     public static void main(String[] args) {
+//        TASK 1
         Vowels     task1 = new Vowels();
+//        TASK 2
         Palindrome task2 = new Palindrome();
+//        TASK 3
         Words      task3 = new Words();
+//        TASK 4
         Consonants task4 = new Consonants();
+//        TASK 5
         LastLetterCount task5 = new LastLetterCount();
+//        TASK 6
         RotateString    task6 = new RotateString();
+//        TASK 7
         Occurrences     task7 = new Occurrences();
+//        TASK 8
         TraverseString  task8 = new TraverseString();
 
-
+//  TASK 1 - Count Vowels in Inout
         do {
                 task1.getInput();
                 task1.showResults(false);
                 task1.showResults(true);
         } while (task1.hasValidInput(true));
 
+//  TASK 2 - Check for Palindromes in Input
         do {
             task2.getInput();
                 task2.checkPalindrome(true);
@@ -28,11 +37,13 @@ public class Main {
                 task2.checkPalindrome(task2.getUserSentence(), SearchMode.WHOLE_SENTENCE_IGNORE_SPACE);
         } while (task2.hasValidInput(true));
 
+//  TASK 3 - Count Words in Input
         do {
                 task3.getInput();
                 task3.displayWordCount(false);
         } while (task3.hasValidInput(true));
 
+//  TASK 4 - Count Consonants in Inout
         do {
             task4.getInput();
             task4.displayConsonantCount(false);
@@ -41,11 +52,13 @@ public class Main {
 
         } while (task4.hasValidInput(true));
 
+//  TASK 5 - Count words that end with A or B, selected by user, also swap the first instance of those words around
         do {
                 task5.getInput();
                 task5.displayLastLetters();
         } while (task5.hasValidInput(true));
 
+//  TASK 6 - Rotate string around by moving current first letter to end of word
         do {
                 task6.getInput();
                 int rotateAmount = task6.getNumber("How many places would you like to shift:");
@@ -53,16 +66,15 @@ public class Main {
                 task6.rotateSentenceWords(rotateAmount);
         } while (task6.hasValidInput(true));
 
+//  TASK 7 - Count unique occurrences if numbers in an array
         do {
                 task7.numericOccurrences(task7.getIntArrayInput());
         } while (task7.hasValidInput(true));
 
+//  TASK 8 - Count and mask Input
         do {
                 task8.getInput();
                 task8.listUniqueCharacters();
         } while (task8.hasValidInput(true));
-
-
-
     }// END OF PSVM
 }// END OF CLASS MAIN
--- Sentence.java
+++ Sentence.java
@@ -6,18 +6,25 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Sentence {
-
-
+//    Variable to store User Input
     private String userSentence;
+//    Scanner to take user input
     Scanner scr = new Scanner(System.in);
+//    Immutable array of Vowels
     protected final char[] VOWELS     = {'a','e','i','o','u'};
+//    Immutable array of Consonants
     protected final char[] CONSONANTS = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
-
+// Fields to handle Task related information
+    //LATER: Encapsulate
   int id;
   String message;
   String description;
 
-
+    /**
+     * TryParse implementation
+     * @param value String value to be parsed as int
+     * @return Parsed integer
+     */
     private int intTryParse(String value){
         try {
             return Integer.parseInt(value);
@@ -27,24 +34,44 @@ public class Sentence {
         }
     }
 
+    /**
+     * Convert each word to String array. Values are separated by space
+     * @return String array of user input
+     */
     protected String[] getInputAsArray(){
         return getUserSentence().split("\\n");
     }
 
+    /**
+     * Get value of user input
+     * @return User Input
+     */
     public String getUserSentence(){
         return Objects.requireNonNullElse(userSentence, "");
     }
 
+    /**
+     * Set user input value
+     * @param sentence Set user input to String
+     */
     public void setUserSentence(String sentence){
         userSentence = sentence;
     }
 
+    /**
+     * Print header and as for user input
+     */
     public void getInput(){
         printHeader();
         String sentence = scr.nextLine();
         setUserSentence(sentence);
     }
 
+    /**
+     * Used for Task  6. User provided integer
+     * @param message Set header message to this before user is able to enter their input
+     * @return integer number
+     */
     public int getNumber(String message){
         if (hasValidInput(false)){
             System.out.printf("%s\n",message);
@@ -53,6 +80,10 @@ public class Sentence {
         return -1;
     }
 
+    /**
+     * Parses 1 or more user provided numbers, separated by space of comma
+     * @return array of integers user provided
+     */
     public int[] getIntArrayInput() {
         printHeader();
         int[] intArray;
@@ -69,6 +100,12 @@ public class Sentence {
         }
         return new int[0];
     }
+
+    /**
+     * Check if user has provided valid input which affects loop and makes sure code is not getting executed with invalid data
+     * @param provideFeedback Provide feedback to user
+     * @return TRUE - user has provided valid input\nFALSE - user has not proided valid input
+     */
     public boolean hasValidInput(boolean provideFeedback) {
         if(getUserSentence().equals("")){
             if(provideFeedback){
@@ -81,15 +118,24 @@ public class Sentence {
         }
     }
 
+    /**
+     * Count the number of words in user input
+     * @param countOnlyUnique Limit count to unique words versus every word
+     * @return words counted in user input
+     */
     protected int getWordCount(boolean countOnlyUnique) {
         if (countOnlyUnique) {
-            //TODO: Count only unique words
+            //LATER: Count only unique words?
             return -1;
         } else {
             return getUserSentence().split("\\s").length;
         }
     }
 
+    /**
+     * Calculate the longest word in user input, used for text padding
+     * @return length of longest word in user input as integer
+     */
     public int getLongestWord() {
         String[] sentence = getUserSentence().replaceAll("\\?!,\\.", "").replaceAll("@", " ").split("\\s");
         int longest = Integer.MIN_VALUE;
@@ -101,19 +147,44 @@ public class Sentence {
         return longest;
     }
 
-
+    /**
+     * PadLeft implementation. User for text formatting, clamps text to left side
+     * @param text text to be padded to the left
+     * @param padAmount  total character size
+     * @return Left-padded text
+     */
     protected String padLeft(String text, int padAmount){
         return String.format("%-" + padAmount + "s",text);
     }
+    /**
+     * PadLeft implementation. User for text formatting, clamps text to right side
+     * @param text text to be padded to the right
+     * @param padAmount  total character size
+     * @return Right-padded text
+     */
     protected String padRight(String text, int padAmount){
         return String.format("%" + padAmount + "s",text);
     }
+
+    /**
+     * User input in list array format
+     * @return List\<\> of user input
+     */
     protected ArrayList<String> getInputList(){
         return new ArrayList<>(Arrays.asList(getUserSentence().split("\\s")));
     }
+
+    /**
+     * Gets first word of user input, even if they entered multiple words
+     * @return First word of user input
+     */
     protected String getInputFirstWord(){
         return getInputList().get(0);
     }
+
+    /**
+     * Provides visual separation and information to user
+     */
     private void printHeader(){
         System.out.printf("================ TASK %d ================\n", id);
         System.out.printf("= %s\n", description);
